e488dde2ccb722736d65fe4700fb49c75d5c369d
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-24 14:33:33 +0100

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -1,11 +1,11 @@
 import requests
 import time
-import testjob.json
+#import testjob.json
 
 def getData():
     time.sleep(0.01)
-    #requests.get("http://0.0.0.0:8080/api/v1/jobs")
-    requests.post("http://0.0.0.0:8080/api/v1/jobs", json=testjob.json, headers={"Content-Type": "application/json"})
+    requests.get("http://0.0.0.0:8080/api/v1/jobs")
+   # requests.post("http://0.0.0.0:8080/api/v1/jobs", json=testjob.json, headers={"Content-Type": "application/json"})
     
     
 getData()    
