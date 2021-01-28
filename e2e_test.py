a06cf80e44e83be3509bc02fd89d7c5f3a0b0967
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-28 09:43:40 +0100

    Update e2e_test.py

--- e2e_test.py
+++ e2e_test.py
@@ -4,5 +4,6 @@ import time
 import sys
 #import testjob.json
 
-return id = {'0bbe1d06-613d-11eb-b3a4-0242ac120006.nc': {'href': 'http://localhost:8080/download/027b4660-613d-11eb-b581-0242ac120004/0bbe1d06-613d-11eb-b3a4-0242ac120006'}}
+id = {'0bbe1d06-613d-11eb-b3a4-0242ac120006.nc': {'href': 'http://localhost:8080/download/027b4660-613d-11eb-b581-0242ac120004/0bbe1d06-613d-11eb-b3a4-0242ac120006'}}
+
 
