47fcd97c7beb369c47745dc607273ac7da90482a
jona159 <65068389+jona159@users.noreply.github.com>
2021-01-28 11:11:28 +0100

    Update e2e_test.py

--- e2e_test.py
+++ e2e_test.py
@@ -9,6 +9,7 @@ def test():
 
   link_json = {'0bbe1d06-613d-11eb-b3a4-0242ac120006.nc': {'href': 'http://localhost:8080/download/027b4660-613d-11eb-b581-0242ac120004/0bbe1d06-613d-11eb-b3a4-0242ac120006'}}
   link = link_json.get(key_new)
+  print(link)
   href = list(link.values())[0]
   print(href)
 
