504f116a0b7351d5cddb893384333bb0ca3e8670
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-28 09:59:06 +0100

    Update e2e_test.py

--- e2e_test.py
+++ e2e_test.py
@@ -6,10 +6,11 @@ rjson = {'jobs': [{'created': '2021-01-28T08:40:47.19Z', 'description': 'Example
 job_id = rjson['jobs'][-1]['id']
 
 json = {'assets': {'8d615358-6144-11eb-ab20-0242ac120006.nc': {'href': 'http://localhost:8080/download/846668b0-6144-11eb-a2b4-0242ac120004/8d615358-6144-11eb-ab20-0242ac120006'}}, 'geometry': None, 'id': '846668b0-6144-11eb-a2b4-0242ac120004', 'links': [], 'properties': {'created': '2021-01-28T08:40:47.19Z', 'datetime': '2021-01-28T08:45:47.29Z', 'description': 'Example Description', 'end_datetime': '2021-01-28T08:41:02.26Z', 'start_datetime': '2021-01-28T08:40:52.18Z', 'title': 'Example Title'}, 'stac_version': '1.0.0', 'type': 'Feature'}
-
+json_id= json['assets'][0]
+print(json_id)
 
 link_json = {'0bbe1d06-613d-11eb-b3a4-0242ac120006.nc': {'href': 'http://localhost:8080/download/027b4660-613d-11eb-b581-0242ac120004/0bbe1d06-613d-11eb-b3a4-0242ac120006'}}
-link = link_json['href']
-print(link)
+#link = link_json['href']
+#print(link)
 
 
