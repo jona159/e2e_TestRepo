5cdc1216c9d6fc0a92895a1ec8a6d31456b85f13
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-29 13:48:50 +0100

    Update e2e_test.py

--- e2e_test.py
+++ e2e_test.py
@@ -2,7 +2,7 @@ import json
 import os
 
 #print(os.environ.items())
-print(os.getenv[‘pw’])
+print(os.getenv[pw])
 
 def test():
   json = {'assets': {'8d615358-6144-11eb-ab20-0242ac120006.nc': {'href': 'http://localhost:8080/download/846668b0-6144-11eb-a2b4-0242ac120004/8d615358-6144-11eb-ab20-0242ac120006'}}, 'geometry': None, 'id': '846668b0-6144-11eb-a2b4-0242ac120004', 'links': [], 'properties': {'created': '2021-01-28T08:40:47.19Z', 'datetime': '2021-01-28T08:45:47.29Z', 'description': 'Example Description', 'end_datetime': '2021-01-28T08:41:02.26Z', 'start_datetime': '2021-01-28T08:40:52.18Z', 'title': 'Example Title'}, 'stac_version': '1.0.0', 'type': 'Feature'}
