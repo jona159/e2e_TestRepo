3eabd2dbe3592629b674f1b075e325b8a81c6e55
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-29 13:53:59 +0100

    Update e2e_test.py

--- e2e_test.py
+++ e2e_test.py
@@ -3,7 +3,7 @@ import os
 # https://github.community/t/can-i-access-the-value-in-an-env-file-from-a-python-script-using-os-getenv/139439/2
 # https://github.community/t/using-secrets-for-code-and-database-credentials/135875/6
 #print(os.environ.items())
-print(os.getenv('SCIHUB_PASSWORD'))
+print(os.getenv('pw'))
 
 def test():
   json = {'assets': {'8d615358-6144-11eb-ab20-0242ac120006.nc': {'href': 'http://localhost:8080/download/846668b0-6144-11eb-a2b4-0242ac120004/8d615358-6144-11eb-ab20-0242ac120006'}}, 'geometry': None, 'id': '846668b0-6144-11eb-a2b4-0242ac120004', 'links': [], 'properties': {'created': '2021-01-28T08:40:47.19Z', 'datetime': '2021-01-28T08:45:47.29Z', 'description': 'Example Description', 'end_datetime': '2021-01-28T08:41:02.26Z', 'start_datetime': '2021-01-28T08:40:52.18Z', 'title': 'Example Title'}, 'stac_version': '1.0.0', 'type': 'Feature'}
