bce3b20b9649ce90fa6e722d0b203e9db92b6bc3
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-01-29 11:36:02 +0100

    Update e2e_sst.py

--- e2e_sst.py
+++ e2e_sst.py
@@ -44,21 +44,11 @@ testjob = {
     }  
 
 def e2e():
-   res = requests.get("http://0.0.0.0:8080/api/v1/jobs")
-#print(res)
-#print(res.text)
-   # requests.post("http://0.0.0.0:8080/api/v1/jobs", json=testjob.json, headers={"Content-Type": "application/json"})
+   res = requests.get("http://0.0.0.0:8080/api/v1/jobs") 
     
-    
- 
-
-# print(testjob)
    print("\n JSON AN FRONTEND ÜBERGEBEN \n")
    x = requests.post("http://0.0.0.0:8080/api/v1/jobs", json=testjob, headers={"Content-Type": "application/json"})
    print(x)
-
-#res_1 = requests.get("http://0.0.0.0:8080/api/v1/jobs")
-#print(res_1.text)
 
    print("\n ID DES JOBS ERFRAGEN \n")
 
@@ -96,62 +86,3 @@ def e2e():
    return replacement
   
 e2e()
-   
-#webUrl = urllib.request.urlretrieve("http://0.0.0.0:8080/download/4be92668-6166-11eb-8f6b-0242ac120004/555369fc-6166-11eb-9729-0242ac120006", "netCDF_File")   
-#os.system('wget %s' %replacement)   
-   
-
-#counter = 0
-'''
-# def callCheckData():
- #  checkData();
-
-def checkData():
-  global counter
-  data = requests.get("http://0.0.0.0:8080/api/v1/jobs/" + job_id + "/results" ).json()
-  if (counter === 3):
-      sys.exit('unable to download data within 15 Minutes')
-  elif(data['level'] == 'error'):
-      print ("Daten noch nicht geladen")
-      counter += 1
-      time.sleep(300)
-      callCheckData()
-  else: 
-      print ("Datensatz geladen")
-      print(requests.get("http://0.0.0.0:8080/api/v1/jobs/" + job_id + "/results" ).json())
-      #res = requests.get("http://localhost/api/v1/jobs/" + job_id + "/results" )
-      #dl = res.json()["assets"]
-      #print(dl)
-
-print("\n WARTEN BIS DATENSATZ GELADEN IST: \n")
-checkData()
-   
-
-#print(" \n GET Anfrage an Frontend /jobRunning Endpoint \n ")
-
-#x2 = requests.get("http://0.0.0.0:8080/jobRunning/" + job_id)
-#print(x2.text)
-
-#print(" \n Post an Database /doJob Endpoint \n ")
-
-#while True:
- # try:
-  #  x3 = requests.post("http://0.0.0.0:8080/doJob/" + job_id)
-   # print(x3.text)
- # except requests.exceptions.RequestException as e:
-  #  pass
-#print(" \n Warte bis Job Fertig ist.. \n")
-#while True:
- 
-#   time.sleep(300) 
- #   try:
-  #    requests.get("http://0.0.0.0:8080/api/v1/jobs/" + job_id + "/results")
-   #   break
-  #  except Error:
-   #     print ("not ready, trying again in one minute")
-         
-#res_2 = requests.get("http://0.0.0.0:8080/api/v1/jobs/" + job_id + "/results").json()
-#d1 = res_2.json()
-#print(d1)
-#print(res_2)
-'''
