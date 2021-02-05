e3e1e9d21e7366138ae32e2c2f59a5ae4b2ff5fc
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-02-05 13:44:20 +0100

    Update index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -6,9 +6,13 @@ const core = require('@actions/core')
 const url = "http://0.0.0.0:80/api/v1"
 let connection = null;
 
+var coll;
+var proc;
+
 console.log('URL: ' + url);
 console.log('Client Version: ' + OpenEO.clientVersion());
 
+function Data(){
 OpenEO.connect(url)
 	.then(c => {
 		connection = c;
@@ -19,13 +23,29 @@ OpenEO.connect(url)
 		return connection.listCollections();
 	})
 	.then(collections => {
+		coll = (collections.collections.length);
 		console.log('Number of supported collections: ' + collections.collections.length);
 		return connection.listProcesses();
 	})
 	.then(processes => {
+		proc = (processes.processes.length);
 		console.log('Number of supported processes: ' + processes.processes.length);
 	})
 	.catch(err => console.error(err.message));
+}
 
+function wait(){
+	Data();
+	setTimeout(() => {
+	if (coll < 0) {
+	   throw new Error('Number of collections must be at least 1);
+			   }
+	else if (proc != 4){
+		   throw new Error('Given number of processes does not match the expected number');
+	   }
+	}, 10000);
+}
+		   
+wait();
 //if (processes.processes.length = 50){
 //core.setFailed('Nicht die korrekte Anzahl der Prozesse', 1)}
