1258f2c1ba04fd71be15db4d311367fd1ef529ed
a2beckj <18323856+a2beckj@users.noreply.github.com>
2021-02-07 00:45:43 +0100

    Update and rename xr.py to xr_ndvi.py

--- xr_ndvi.py
+++ xr_ndvi.py
@@ -12,15 +12,6 @@ import pytest
 from netCDF4 import Dataset
 import h5py
 
-fin_sst = xr.open_dataset('SST_datacube.nc')
-print(fin_sst)
-
-sst_values = fin_sst['sst'][0]
-print(sst_values)
-
-
-def test_length_fin_sst():
-   assert fin_sst.count() == 1036800
    
 fin_ndvi = xr.open_dataset('Sentinel_datacube.nc')
 print(fin_ndvi)   
