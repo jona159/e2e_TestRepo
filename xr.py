c03de81e833f3d3139690ea0503ac19417ee0bfd
jona159 <65068389+jona159@users.noreply.github.com>
2021-02-07 00:28:04 +0100

    Update xr.py

--- xr.py
+++ xr.py
@@ -12,12 +12,24 @@ import pytest
 from netCDF4 import Dataset
 import h5py
 
-fin = xr.open_dataset('SST_datacube.nc')
+fin_sst = xr.open_dataset('SST_datacube.nc')
 print(fin)
 
-sst_values = fin['sst'][0]
+sst_values = fin_sst['sst'][0]
 print(sst_values)
 
 
-def test_length_fin():
-   assert fin.count() == 1036800
+def test_length_fin_sst():
+   assert fin_sst.count() == 1036800
+   
+fin_ndvi = xr.open_dataset('Sentinel_datacube.nc')
+print(fin_ndvi)   
+   
+print(fin_ndvi['__xarray_dataarray_variable__'][:])
+
+print(" \n END OF NDVI NETCDF FILE \n")
+
+# Test to assert that the xarray Dataset contains the correct number of values
+def test_length_fin_ndvi():
+  assert fin_ndvi.count() == 2303604   
+   
