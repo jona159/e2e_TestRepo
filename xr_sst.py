ac9c7f27099e2945dc1a708b36d22df63b180b06
jona159 <65068389+jona159@users.noreply.github.com>
2021-02-08 20:45:46 +0100

    Update xr_sst.py

--- xr_sst.py
+++ xr_sst.py
@@ -1,3 +1,6 @@
+#@author Judith Becka, https://github.com/a2beckj
+#@author Jonas Raabe, https://github.com/jona159
+
 import requests
 import json
 import time
@@ -12,12 +15,14 @@ import pytest
 from netCDF4 import Dataset
 import h5py
 
+# open xarray dataset
 fin_sst = xr.open_dataset('SST_datacube.nc')
 print(fin_sst)
+
 
 sst_values = fin_sst['sst'][0]
 print(sst_values)
 
-
+# Test correct amount of values in xarray dataset
 def test_length_fin_sst():
    assert fin_sst.count() == 1036800
